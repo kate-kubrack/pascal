07fcdfdfc1541893675311989d7b647f53be86df
Kate Kubrak <k.kubrack@gmail.com>
2020-11-12 21:34:38 +0200

    oreshki.pas task added

---
+++ oreshki.pas
@@ -0,0 +1,20 @@
+var
+	N, M, K: integer;
+	f_input, f_output : text;
+begin
+	assign(f_input, 'INPUT.TXT');
+	assign(f_output, 'OUTPUT.TXT');
+
+	reset(f_input);
+	rewrite(f_output);
+
+	read(f_input, N);
+	read(f_input, M);
+	read(f_input, K);
+
+	if N*M<K then write(f_output, 'NO')
+	else write(f_output, 'YES');
+
+	close(f_input);
+	close(f_output);
+end.
